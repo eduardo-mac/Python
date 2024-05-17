1a3b377d6d031fedf79afc27555d580434539f94
Eduardo <eduardosilvamacedo96@gmail.com>
2024-05-17 20:09:50 -0300

    tuplas

---
+++ Python/tuplas.py
@@ -0,0 +1,14 @@
+# Sao imutáveis
+
+# Operações não disponíveis em tuplas: .sort(), .append(), .reverse(), .pop()...
+
+elementos = ('F', 'Cl', 'Br', 'I','At')
+
+# for elemento in elementos:
+#     print(f'\nElemento Químico: {elemento}')
+
+#criar uma lista atraves de um tupla
+
+grupo17 = list(elementos)
+grupo17.append('Ox')
+print(grupo17)
